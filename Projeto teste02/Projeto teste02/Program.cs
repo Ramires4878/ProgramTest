6c672b4e8ffb6b7060921bb7e82b6cbdb844220d
Ramires Oliveira <ramiresoliveira.10@hotmail.com>
2020-07-02 21:57:00 -0300

    inseri um writeline

--- Projeto teste02/Projeto teste02/Program.cs
+++ Projeto teste02/Projeto teste02/Program.cs
@@ -7,6 +7,7 @@ namespace Projeto_teste02
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("odsokdas");
         }
     }
 }
